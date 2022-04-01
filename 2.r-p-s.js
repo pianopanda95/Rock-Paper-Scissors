4e99b9ec7d3e50b37156e09c8f1285e323d28fca
pianopanda <kramerfanni95@gmail.com>
2022-04-01 12:08:37 +0200

    reduce code from r-p-s.js

---
+++ 2.r-p-s.js
@@ -0,0 +1,64 @@
+
+const rps = ["rock", "paper", "scissors"];
+const invalid = "You must choose 'rock', 'paper' or 'scissors'!";
+
+let playerPoints = 0;
+let compPoints = 0;
+let drawPoints = 0;
+
+
+function game(){
+    for (let counter = 1; counter < 6; counter++){
+        const playerSelection = prompt(`\nRound ${counter}/5\nROCK, PAPER, SCISSORS!\nGO!\n`);
+        if (rps.includes(playerSelection.toLowerCase())){
+            playRound(playerSelection);
+        } else {
+            console.log(invalid);
+            counter -= 1;
+        }
+    }
+    playerPoints = compPoints = drawPoints = 0;
+}
+
+
+function playRound(playerSelection, compSelection = computerPlay()){
+    console.log(`The computer chose: ${compSelection}.`);
+    if (playerSelection == compSelection){
+            draw();
+    } else if (playerSelection == "rock"){
+            (compSelection == "scissors") ?
+            win():
+            lose();
+    } else if (playerSelection == "paper"){
+            (compSelection == "rock") ?
+            win():
+            lose();
+    } else {
+            (compSelection == "paper") ?
+            win():
+            lose();
+    }
+    console.log(playerPoints, drawPoints, compPoints);
+}
+
+
+function win(){
+    console.log("You won this round!");
+    playerPoints += 1;
+}
+function lose(){
+    console.log("You lost this round!");
+    compPoints += 1;
+}
+function draw(){
+    console.log("It's a draw!");
+    drawPoints += 1;
+}
+
+
+function computerPlay(){
+    const randomIndex = Math.floor(Math.random() * rps.length);
+    const compSelection = rps[randomIndex];
+    return compSelection;
+    }
+    
